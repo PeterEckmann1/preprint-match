fd7a77749a40e562a02a966257b695fb7730a5d9
PeterEckmann1 <53533143+PeterEckmann1@users.noreply.github.com>
2019-12-22 22:35:15 -0800

    Add files via upload

---
+++ database/create_matrix_master.py
@@ -0,0 +1,40 @@
+import subprocess
+import os
+import time
+
+os.system('del matrices\\*.npz')
+
+running_procs = 0
+prev_file_count = 0
+
+starting = range(0, len(os.listdir('pubmed3')), 100)[:-1]
+i = 0
+
+while i < len(starting):
+    if running_procs < 1:
+        subprocess.Popen(['python', 'create_matrix_slave.py', str(starting[i]), str(starting[i] + 100), '0'])
+        subprocess.Popen(['python', 'create_matrix_slave.py', str(starting[i]), str(starting[i] + 100), '1'])
+        subprocess.Popen(['python', 'create_matrix_slave.py', str(starting[i]), str(starting[i] + 100), '2'])
+        running_procs += 3
+        i += 1
+
+    if len(os.listdir('matrices')) / 2 != prev_file_count:
+        running_procs -= len(os.listdir('matrices')) / 2 - prev_file_count
+        prev_file_count = len(os.listdir('matrices')) / 2
+
+    time.sleep(1)
+
+subprocess.Popen(['python', 'create_matrix_slave.py', str(len(os.listdir('pubmed3')) - len(os.listdir('pubmed3')) % 100), str(len(os.listdir('pubmed3'))), '0'])
+subprocess.Popen(['python', 'create_matrix_slave.py', str(len(os.listdir('pubmed3')) - len(os.listdir('pubmed3')) % 100), str(len(os.listdir('pubmed3'))), '1'])
+subprocess.Popen(['python', 'create_matrix_slave.py', str(len(os.listdir('pubmed3')) - len(os.listdir('pubmed3')) % 100), str(len(os.listdir('pubmed3'))), '2'])
+
+while int(len(os.listdir('matrices')) / 6) * 100 < len(os.listdir('pubmed3')):
+    time.sleep(1)
+
+exit(0)
+for f_name in os.listdir('matrices'):
+    if 'pmids' in f_name:
+        if 'abstract' in f_name:
+            os.rename('matrices/' + f_name, 'matrices/' + f_name[8:])
+        else:
+            os.remove('matrices/' + f_name)
