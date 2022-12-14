188af2dfa8341f515b1edcdc5208553fd6c2c0ac
PeterEckmann1 <53533143+PeterEckmann1@users.noreply.github.com>
2022-12-14 13:39:31 -0800

    Update load_pubmed.py

--- load_pubmed.py
+++ load_pubmed.py
@@ -5,26 +5,30 @@ import numpy as np
 import os
 import json
 
-#todo can become bloated after hundreds of files, need restart, HAVE TO REDO VECTORS
+
 if __name__ == '__main__':
-    #processed_files = set(json.loads(open('processed_files.json', 'r').read()))
-    #all_files = ['data/pubmed/' + file for file in os.listdir('data/pubmed')]
-    #print([file for file in all_files if file not in processed_files])
-    #pubmed = PubMed([file for file in all_files if file not in processed_files])
+    if os.path.exists('processed_files.json'):
+        processed_files = set(json.loads(open('processed_files.json', 'r').read()))
+    else:
+        processed_files = []
+    if not os.path.exists('data/pubmed'):
+        os.mkdir('data/pubmed')
+    all_files = ['data/pubmed/' + file for file in os.listdir('data/pubmed')]
+    print([file for file in all_files if file not in processed_files])
+    pubmed = PubMed([file for file in all_files if file not in processed_files])
     db = Database('postgres', 'testpass', port=5434)
-    #db.delete()
-    #for i, paper in enumerate(pubmed.stream_papers()):
-    #    db.insert(paper['doi'], paper['title'], paper['abstract'], paper['authors'], paper['date'])
-    #    if i % 10001 == 0:
-    #        processed_files = list(set(pubmed.processed_files).union(processed_files))
-    #        open('processed_files.json', 'w').write(json.dumps(processed_files))
-    #db.commit()
-    #processed_files = list(set(pubmed.processed_files).union(processed_files))
-    #open('processed_files.json', 'w').write(json.dumps(processed_files))
-    #db.generate_word_vectors('data', 1)
-    #db.apply_word_vectors('data')
+    db.delete()
+    for i, paper in enumerate(pubmed.stream_papers()):
+        db.insert(paper['doi'], paper['title'], paper['abstract'], paper['authors'], paper['date'])
+        if i % 10001 == 0:
+            processed_files = list(set(pubmed.processed_files).union(processed_files))
+            open('processed_files.json', 'w').write(json.dumps(processed_files))
+    db.commit()
+    processed_files = list(set(pubmed.processed_files).union(processed_files))
+    open('processed_files.json', 'w').write(json.dumps(processed_files))
+    db.generate_word_vectors('data', 1)
+    db.apply_word_vectors('data')
     db.generate_matrices('data')
-    exit()
     matcher = Matcher('data', db)
     vectors = Vectors('data')
 
